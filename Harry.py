0efc4bf0b1e6239020d10acfcb56eb9accead37f
Harpreet <137159929+Harpreet8920@users.noreply.github.com>
2025-10-01 22:41:46 +0530

    Update Harry.py

--- Harry.py
+++ Harry.py
@@ -1,7 +1,7 @@
 def addition(a:int,b:int,c:int):
     return a+b+c
-def subtraction(a:int,b:int):
-    return a-b
+def subtraction(a:int,b:int,c:int):
+    return a-b-c
 def multiply(a,b):
     return a*b
 def division(a,b):
